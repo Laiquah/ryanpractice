29952450c9d554ed7b3a9131ff96f128a7fa5979
Laiquah <cotielaiquah@gmail.com>
2023-06-09 09:06:57 +0200

    dfghj

---
+++ script/code.js
@@ -0,0 +1,98 @@
+// async function fetchMe(){
+//     fetch('https://api.publicapis.org/entries')
+//     .then((res)=>res.json())
+//     .then((data)=>console.log(data))
+// }
+
+// fetchMe()
+
+// fetch("https://api.publicapis.org/entries")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// function testFunction() {
+//     return new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         resolve("hello!");
+//         reject("BYE");
+//       }, 1000);
+//     });
+//   }
+
+// testFunction()
+
+// Create an array of Objects (At least 3 Objects). Each object should have a URL Link to an image.
+// Set your array in localStorage.
+// Get your array from localStorage and store it in a variable called "Information"
+// Display/Write the content of the variable "Information" on the document and make sure the images are displayed
+
+// x=5;
+// if (x>2){
+
+// console.log(x +" is greater than 2")
+
+// }else
+
+// {
+
+// console.log(x + " is less than 2")
+
+// };
+
+// let mark=5;
+
+// let result=mark>5 ? "weak":"strong";
+// console.log(result)
+
+// let a=1;
+// if(a>10)
+// {  
+//     document.write(10);  
+// } 
+// else
+// {
+//     document.write(a);  
+// }
+
+// let accessAllowed;
+// let age = 12;
+
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+
+// console.log(accessAllowed)
+
+// function testNum(a) {
+//     let result;
+//     if (a > 0) {
+//       result = 'positive';
+//     } else {
+//       result = 'NOT positive';
+//     }
+//     return result;
+//   }
+  
+//   console.log(testNum(-5));
+
+// let ages_1 = 20, ages_2='20';
+// if(ages_1 === ages_2){
+//   alert('same');
+// }
+// else{ alert('not same');}
+
+// console.log(ages_1)
+
+let i = 10;
+  if (i == 10) {
+    if (i < 15)
+    document.write(i + " is smaller than 15");
+    if (i < 12)
+    document.write(i +" is smaller than 12 too");
+  else
+    document.write(i + " is greater than 15");
+}
+
+console.log(i)
